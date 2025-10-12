4c9de9ca56a00ff0576e02d2a6fd902684fd6a47
Elbert Timothy <ignatiuselbert5@gmail.com>
2025-10-12 13:48:29 +0100

    cleaned up dir

--- src/typed-schemas.ts
+++ src/typed-schemas.ts
@@ -1,6 +1,6 @@
 import * as z from "zod/v4";
 import * as core from "zod/v4/core";
-import type { MapTypeToZodType } from "./zod-types";
+import type { MapTypeToZodType } from "./map-type-to-zod-type";
 import type { Merge } from "type-fest";
 
 export const createTypedStrictObject =
@@ -30,33 +30,3 @@ export const createTypedLooseObject =
       { in: T; out: Merge<T, z.output<ShapeType>> }
     >;
   };
-
-type T = {
-  a: string;
-  b: string;
-  c: boolean;
-  x: [1, 2];
-  obj: {
-    d: string[];
-  };
-};
-
-createTypedStrictObject<T>()({
-  a: z.string(),
-  b: z.string(),
-  c: z.boolean(),
-  x: z.tuple([z.literal(1), z.literal(2)]),
-  obj: z.object({
-    d: z.array(z.string()).transform((arr) => arr.map((i) => parseInt(i))),
-  }),
-}).transform(({ a, b, c, x, obj }) => ({ a, b, c, x }));
-
-createTypedLooseObject<T>()({
-  a: z.string().transform((v) => parseInt(v)),
-  c: z.boolean(),
-  obj: z
-    .object({
-      d: z.array(z.string()),
-    })
-    .transform(({ d }) => ({ d_trans: d.map((i) => parseInt(i)) })),
-}).transform(({ a, b, c, obj, ...rest }) => {});
