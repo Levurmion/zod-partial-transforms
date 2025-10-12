e37c638157f34b49ece0bb5d3fff27faef8b5825
Elbert Timothy <ignatiuselbert5@gmail.com>
2025-10-12 13:52:27 +0100

    created typed generics

--- src/typed-schemas.ts
+++ src/typed-schemas.ts
@@ -3,6 +3,14 @@ import * as core from "zod/v4/core";
 import type { MapTypeToZodType } from "./map-type-to-zod-type";
 import type { Merge } from "type-fest";
 
+export type TypedStrictObject<T extends core.$ZodLooseShape> = <
+  Shape extends {
+    [K in keyof T]: MapTypeToZodType<T[K]>;
+  }
+>(
+  shape: Shape
+) => z.ZodObject<Shape, core.$strict>;
+
 export const createTypedStrictObject =
   <T extends core.$ZodLooseShape>() =>
   <
@@ -14,6 +22,20 @@ export const createTypedStrictObject =
   ) => {
     return z.strictObject(shape);
   };
+
+export type TypedLooseObject<T extends core.$ZodLooseShape> = <
+  Shape extends Partial<{
+    [K in keyof T]: MapTypeToZodType<T[K]>;
+  }>
+>(
+  shape: Shape
+) => z.ZodObject<
+  Extract<Shape, core.$ZodShape>,
+  {
+    in: T;
+    out: Merge<T, z.output<z.ZodObject<Extract<Shape, core.$ZodShape>>>>;
+  }
+>;
 
 export const createTypedLooseObject =
   <T extends core.$ZodLooseShape>() =>
